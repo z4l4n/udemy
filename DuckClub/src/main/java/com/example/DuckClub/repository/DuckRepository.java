837aa3e8d6ac6c9e4b151b1f8532857c61621334
Vajda Zalán <z4l4n@hotmail.com>
2024-04-15 14:59:07 +0200

    asdasd

--- DuckClub/src/main/java/com/example/DuckClub/repository/DuckRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DuckClub.repository;
-
-import com.example.DuckClub.entity.Duck;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DuckRepository extends JpaRepository<Duck, Long> {
-}
